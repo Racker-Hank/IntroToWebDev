9e33433c5af9c37f4e1b4e3f80a44bd9d29eccc1
Racker-Hank <82131995+Racker-Hank@users.noreply.github.com>
2022-02-16 20:35:01 +0700

    hero 2

--- code/intro-to-web-dev/src/components/hero/index.js
+++ code/intro-to-web-dev/src/components/hero/index.js
@@ -5,28 +5,11 @@ const Hero = () => {
 		<section className='hero-section' id='hero'>
 			<div className='hero-container'>
 				<div className='hero-text-container'>
-					<div className='small-text-container'>
-						<p className='small-text'>
-							create your
-							<span id='first-website'> first website</span>
-							<span id='yourself'> yourself</span>
-						</p>
-						<svg
-							id='line4'
-							width='61'
-							height='7'
-							viewBox='0 0 61 7'
-							fill='none'
-							xmlns='http://www.w3.org/2000/svg'
-						>
-							<path
-								d='M2.29899 5.0551C28.3748 -2.12519 32.5318 10.3458 59.3634 2.03182'
-								stroke='#5378FF'
-								stroke-width='3'
-								stroke-linecap='round'
-							/>
-						</svg>
-					</div>
+					<p className='small-text'>
+						create your
+						<span id='first-website'> first website</span>
+						<span id='yourself'> yourself</span>
+					</p>
 					<h1 className='big-text' id='learn'>
 						learn it yourself
 					</h1>
