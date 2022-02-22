b287768128db7d9dc38054873afeb982b9f6b7fb
Racker-Hank <82131995+Racker-Hank@users.noreply.github.com>
2022-02-22 18:27:11 +0700

    whoopsie

--- code/intro-to-web-dev/src/components/channel/index.js
+++ code/intro-to-web-dev/src/components/channel/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import SectionBreakText from '../section-break-text';
 import Roadmap from './roadmap';
 import CrashCourses from './crash-course';
 import FrontEnd from './front-end';
@@ -8,6 +9,12 @@ import WebDev from './web-dev';
 const Channel = () => {
 	return (
 		<section id='channel' style={{ paddingTop: '5rem' }}>
+			<SectionBreakText
+				section='channel-start'
+				title='channels to help you out'
+				subtitles={['Amazing', 'sharing their knowledge']}
+				redText='creators'
+			/>
 			<Roadmap />
 			<CrashCourses />
 			<FrontEnd />
